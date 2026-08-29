948e4ff4b51bb46a9e305d9213fe37058e3357de
kayo_ms <kayo.malafaiaa@gmail.com>
2026-08-29 09:54:54 -0300

    criado o primeiro servidor com o express

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aula_01_nodejss
 aula 00 de node - Beckend em js
+# aula_01_node se o cmd bloquear 
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+espaço na memoria usado para armazenar dados
+
+# comandos basicos
+- git add -
+- git commit -m "Criado o primeiro servidor com  o express"
+- git push
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello Word !")
+import express from 'express'
+
+const app = express()
+
+//req = requisição , res= resposta .
+app.get('/busca', (req, res) => {
+    console.log(req)
+  res.send('Olá Mundo')
+})
+
+
+app.get('/busca/:id', (req, res) => {
+    const numero = req.params.id
+    console.log(numero)
+    const mensagem = `O número requisitado é :${numero}`
+
+  res.send(mensagem)
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
